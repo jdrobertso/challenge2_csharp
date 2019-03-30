e27618c29e26018ec6e72b3f2bcd4286b42d1ac5
Jeremy Robertson <jrobertson.88@gmail.com>
2019-03-29 23:58:14 -0500

    Initialize project

---
+++ CS-ASP_008/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_008.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            background-color: #008080;
+            font-size: large;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            First Value:&nbsp;
+            <asp:TextBox ID="inputFirstValue" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Second Value: <asp:TextBox ID="inputSecondValue" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="additionButton" runat="server" OnClick="Button1_Click" Text="+" />
+&nbsp;
+            <asp:Button ID="subtractionButton" runat="server" OnClick="subtractionButton_Click" Text="-" />
+&nbsp;
+            <asp:Button ID="multiplicationButton" runat="server" OnClick="multiplicationButton_Click" Text="*" />
+&nbsp;
+            <asp:Button ID="divisionButton" runat="server" OnClick="divisionButton_Click" Text="/" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" BackColor="#99CCFF" Font-Bold="True" Font-Size="Larger"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_008/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_008
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string firstValue = inputFirstValue.Text;
+            string secondValue = inputSecondValue.Text;
+            int valueOne = int.Parse(firstValue);
+            int valueTwo = int.Parse(secondValue);
+
+            int sum = valueOne + valueTwo;
+
+            string result = "The result of your addition is " + sum;
+            resultLabel.Text = result;
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = inputFirstValue.Text;
+            string secondValue = inputSecondValue.Text;
+            int valueOne = int.Parse(firstValue);
+            int valueTwo = int.Parse(secondValue);
+
+            int sub = valueOne - valueTwo;
+
+            string result = "The result of your addition is " + sub;
+            resultLabel.Text = result;
+        }
+
+        protected void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = inputFirstValue.Text;
+            string secondValue = inputSecondValue.Text;
+            int valueOne = int.Parse(firstValue);
+            int valueTwo = int.Parse(secondValue);
+
+            int product = valueOne * valueTwo;
+
+            string result = "The result of your addition is " + product;
+            resultLabel.Text = result;
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = inputFirstValue.Text;
+            string secondValue = inputSecondValue.Text;
+            int valueOne = int.Parse(firstValue);
+            int valueTwo = int.Parse(secondValue);
+
+            int div = valueOne / valueTwo;
+
+            string result = "The result of your addition is " + div;
+            resultLabel.Text = result;
+        }
+    }
+}
